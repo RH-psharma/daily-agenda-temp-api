bee864f398b2bfa286c373c60e81a7619c72d1b1
PrachiZensar <40391209+PrachiZensar@users.noreply.github.com>
2018-07-26 17:07:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -132,21 +132,20 @@ app.get('/associates/12', function(req, res, next) {
 app.get('/get-wekly-events', function(req, res, next) {
 
   var weeklyEvents  = [
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:20},
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:40},
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:50},
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:60},
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:70},
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:80},
-      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"9AM - 6PM", section_id:90},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"[09:00AM - 06:00PM]", section_id:20},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"sick", section_id:40},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"[09:00AM - 06:00PM]", section_id:50},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"vacation", section_id:60},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"[09:00AM - 06:00PM]", section_id:70},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"[09:00AM - 06:00PM]", section_id:80},
+      { start_date: "2018-07-24 00:00", end_date: "2018-07-24 24:00", text:"[09:00AM - 06:00PM]", section_id:90},
 
-      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"9AM - 6PM", section_id:40},
-      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"9AM - 6PM", section_id:50},
-      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"9AM - 6PM", section_id:60},
-      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"9AM - 6PM", section_id:80},
-      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"9AM - 6PM", section_id:90}
+      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"temporary", section_id:40},
+      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"[09:00AM - 06:00PM]", section_id:50},
+      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"[09:00AM - 06:00PM]", section_id:60},
+      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"[09:00AM - 06:00PM]", section_id:80},
+      { start_date: "2018-07-18 00:00", end_date: "2018-07-18 24:00", text:"[09:00AM - 06:00PM,08:00PM - 11:00PM]", section_id:90}
 ];
-
 var storeOpeningHours = [
 {date:"2018-07-15", opening_hours:"9AM - 6PM"},
 {date:"2018-07-16", opening_hours:"9AM - 6PM"},
