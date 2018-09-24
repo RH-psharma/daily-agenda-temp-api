59c3f818b26162650404a96ae1fbb1d8cfe7ea60
PrachiZensar <40391209+PrachiZensar@users.noreply.github.com>
2018-09-24 14:59:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(express.bodyParser());
+
 //API to get employee data
 app.post('/get-employee-details', function(req, res, next) {
    res.send({
    "msg": "Store data",
    "success": true,
-   "data": "https://semantic-ui.com/images/avatar/large/elliot.jpg,John,Doe"
+   "data": "https://semantic-ui.com/images/avatar/large/elliot.jpg,John,Doe",
+     "id":req.body.id
   });
 //   res.send({
 //    "msg": "Store data",
