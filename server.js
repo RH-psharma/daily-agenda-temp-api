fac8d3fefb67132b7b16a850cbabff17c6683aa2
PrachiZensar <40391209+PrachiZensar@users.noreply.github.com>
2018-09-25 17:58:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,13 +27,7 @@ app.get('/forecastAndGoals/:date/:shop-id', function(req, res, next) {
    res.send({
    "msg": "Store data",
    "success": true,
-   "data": [{
-   		"yesterdays_actual" : "15760",
-      "week_to_date_actual" : "16789",
-      "ly_demand" : "16789",
-      "todays_sales" : "56600",
-      "sales_goal" : "60000"
-	  }]
+   "data": "dataaaaaaa"
   });
 });
 
